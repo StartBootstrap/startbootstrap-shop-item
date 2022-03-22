432a542599481483cab7363ec3ba963ced4833c9
David Miller <hello@davidmiller.io>
2022-03-22 16:36:40 -0400

    dependencies update

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Shop Item v5.0.4 (https://startbootstrap.com/template/shop-item)
-* Copyright 2013-2021 Start Bootstrap
+* Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-item/blob/master/LICENSE)
 */
 // This file is intentionally blank
