7eee6160f7960c64d91431deeec7cac2457e689f
David Miller <hello@davidmiller.io>
2023-03-25 18:12:54 -0400

    Bootstrap 5.2.3 dependency update

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
-* Start Bootstrap - Shop Item v5.0.5 (https://startbootstrap.com/template/shop-item)
-* Copyright 2013-2022 Start Bootstrap
+* Start Bootstrap - Shop Item v5.0.6 (https://startbootstrap.com/template/shop-item)
+* Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-item/blob/master/LICENSE)
 */
 // This file is intentionally blank
